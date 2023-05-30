e8a6b0b1cb787b7a52b7871632d1d34cb92e43f9
Alexksksss <sasha.litvak02@mail.ru>
2023-05-30 15:52:56 +0700

    исправления

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@ int main()
     IOCContainer injector;
 
         //------Example #1----------------
-    // Регистрируем IProcessor с классом AMD, т.о. каждый раз запрашивая Iprocessor получаем объект AMD.
+    // Регистрируем IProcessor с классом Intel, т.о. каждый раз запрашивая Iprocessor получаем объект Intel.
     injector.RegisterInstance<IProcessor, IntelProcessor>();
     injector.GetObject<IProcessor>()->SetterProcessor("Intel",x64,2.1);
     Computer computerIntel(injector.GetObject<IProcessor>().get());
     cout<<computerIntel.GetProcessor()<<endl;
 
     //------Example #2----------------
-    // Регистрируем IProcessor с классом Intel, т.о. каждый раз запрашивая Iprocessor получаем объект Inte;.
+    // Регистрируем IProcessor с классом AMD, т.о. каждый раз запрашивая Iprocessor получаем объект Amd;
     injector.RegisterInstance<IProcessor, AMDProcessor>();
     injector.GetObject<IProcessor>()->SetterProcessor("AMD",x86,4.2);
     Computer computerAMD(injector.GetObject<IProcessor>().get());
